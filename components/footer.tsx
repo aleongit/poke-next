dac39c83623aceb77d5646c2804eb0869058438e
aleongit <100855283+aleongit@users.noreply.github.com>
2022-10-28 13:33:59 +0200

    test git

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,9 +6,6 @@ const Footer = () => {
     <>
       <footer className={styles.footer}>
         <p>Powered by <b>aleon</b></p>
-        <span className={styles.logo}>
-          <Image src="/poke.svg" alt="Pokemon Logo" width={25} height={25} />
-        </span>
       </footer>
     </>
   );
